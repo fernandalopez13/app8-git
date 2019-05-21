66db53cee17d9e271d6a6050622d1c6052630247
fernandalopez13 <cb16.lopez.pineda.marifer@gmail.com>
2019-05-21 11:39:12 -0500

    3subir

--- js/acciones.js
+++ js/acciones.js
@@ -22,8 +22,8 @@ $(document).ready(function(e) {
 	 
 	 function onSuccess(position){
 		 
-		 alert ('Latitude:'   +position.coords.latitude   +'\n'+
-		 'Longitude:'     +position.coords.longitude   +'\n');
+		 alert ('Latitud:'   +position.coords.latitude   +'\n'+
+		 'Longitud:'     +position.coords.longitude   +'\n');
 	 };
 	 
 	 function onError (error) {
